c99837ae3ced3ec440903e8a39ee3dc8dac5e603
Jodi Pranata <jodranata@gmail.com>
2020-02-06 14:45:57 +0700

    Updates

---
+++ precache-manifest.264c72bb08a31e8c8561790e5b931bba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98e0f50497558f938ca5cd722824bd3",
+    "url": "/FCC-calculator/index.html"
+  },
+  {
+    "revision": "10fe9bcdb5db921bf537",
+    "url": "/FCC-calculator/static/css/main.e0b9b518.chunk.css"
+  },
+  {
+    "revision": "7a71e0a4e40c05911cb3",
+    "url": "/FCC-calculator/static/js/2.2e537af1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FCC-calculator/static/js/2.2e537af1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10fe9bcdb5db921bf537",
+    "url": "/FCC-calculator/static/js/main.43564216.chunk.js"
+  },
+  {
+    "revision": "da59459ba08d5c20e898",
+    "url": "/FCC-calculator/static/js/runtime-main.ba2aa587.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/FCC-calculator/precache-manifest.264c72bb08a31e8c8561790e5b931bba.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FCC-calculator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
